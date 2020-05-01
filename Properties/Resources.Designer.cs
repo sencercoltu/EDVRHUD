5b096c4f844aa86b2cc7f4955017ff6c5a34ebe7
Sencer Coltu <sencercoltu@gmail.com>
2020-05-01 22:32:28 +0100

    added default panel and settings as resource

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -151,6 +151,16 @@ namespace EDVRHUD.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] Panels {
+            get {
+                object obj = ResourceManager.GetObject("Panels", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap Planet {
@@ -177,6 +187,16 @@ namespace EDVRHUD.Properties {
             get {
                 object obj = ResourceManager.GetObject("Scan", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] Settings {
+            get {
+                object obj = ResourceManager.GetObject("Settings", resourceCulture);
+                return ((byte[])(obj));
             }
         }
         
